c92b9cbb4b2d715c7cb8bdde158caa841b53938f
Sahir <sahirsyatha77@gmail.com>
2021-05-05 23:57:53 +0700

    added api client

--- app/src/main/java/com/dicoding/livedatawithapi/MainActivity.kt
+++ app/src/main/java/com/dicoding/livedatawithapi/MainActivity.kt
@@ -1,11 +1,51 @@
 package com.dicoding.livedatawithapi
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.ArrayAdapter
+import androidx.activity.viewModels
+import com.bumptech.glide.Glide
+import com.dicoding.livedatawithapi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val mainViewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        mainViewModel.restaurant.observe(this, { restaurant ->
+            binding.tvTitle.text = restaurant.name
+            binding.tvDescription.text = restaurant.description
+            Glide.with(this)
+                .load("https://restaurant-api.dicoding.dev/images/large/${restaurant.pictureId}")
+                .into(binding.ivPicture)
+        })
+
+        mainViewModel.listReview.observe(this, { consumerReviews ->
+            val listReview = consumerReviews.map {
+                "${it.review}\n- ${it.name}"
+            }
+            binding.lvReview.adapter = ArrayAdapter(this, R.layout.item_review, listReview)
+            binding.edReview.setText("")
+        })
+
+        mainViewModel.isLoading.observe(this, {
+            binding.progressBar.visibility = if (it) View.VISIBLE else View.GONE
+        })
+
+        binding.btnSend.setOnClickListener { view ->
+            mainViewModel.postReview(binding.edReview.text.toString())
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
+        }
     }
 }
